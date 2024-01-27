ccef6d7e96d1ec17267c704897df34e0eda7b247
rahulgore34 <rahulgore34@gmail.com>
2024-01-27 19:26:11 +0530

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './students/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'students',
+        component: DashboardComponent
+    }
+];
